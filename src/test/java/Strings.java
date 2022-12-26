14d67322154bbfc8772fb9231ae910326a978c87
Nikita <samsometro@gmail.com>
2022-12-26 13:58:48 +0200

    StringsAdd

---
+++ src/test/java/Strings.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+
+public class Strings {
+    @Test
+    public void testSimpleStrings(){
+        String sourceStr = "Hello! My name is Mikaka Dimensions";
+        System.out.println(sourceStr.length());
+
+        System.out.println(sourceStr.charAt(3));
+
+        System.out.println(sourceStr.substring(18));
+
+        System.out.println(sourceStr.substring(7, 17));
+
+        System.out.println(sourceStr.substring(sourceStr.length()-17));
+
+        System.out.println(sourceStr.contains("byaka"));
+
+        System.out.println(sourceStr.contains("!"));
+
+
+    }
+}
