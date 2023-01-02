d6edfdeedb82efbf8902c30e6d7c92981ac73bd7
Nikita <samsometro@gmail.com>
2023-01-02 14:38:23 +0200

    testWebDriver update(page object)

--- src/test/java/testWebDriver/TestInit.java
+++ src/test/java/testWebDriver/TestInit.java
@@ -1,10 +1,11 @@
 package testWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,18 +14,27 @@ public class TestInit {
 
 
     @BeforeMethod
-    public void setupDriver(){
+    public void setupDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().window().setSize(new Dimension(1920, 1080)) ;
+        driver.manage().window().setSize(new Dimension(1920, 1080));
     }
+
     @AfterMethod(alwaysRun = true)
     public void quitDriver() {
         driver.quit();
     }
 
-    public void openURL(String url){
+    public void openURL(String url) {
         driver.get(url);
+    }
+
+    public WebElement getSearchField() {
+        return driver.findElement(By.xpath("//input[@name='q']"));
+    }
+
+    public WebElement getButton() {
+        return driver.findElement(By.xpath("//a[text()='Gmail']"));
     }
 
 }
--- src/test/java/testWebDriver/TestWebDriver.java
+++ src/test/java/testWebDriver/TestWebDriver.java
@@ -1,21 +1,19 @@
 package testWebDriver;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-import testWebDriver.TestInit;
 
 public class TestWebDriver extends TestInit {
 
     @Test
     public void runGoogle() {
         openURL("https://www.google.com.ua");
-        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Zbudny Chas\n");
+        getSearchField().sendKeys("Zbudny Chas\n");
     }
 
     @Test
     public void clickGmailBtn() {
         openURL("https://www.google.com.ua");
-        driver.findElement(By.xpath("//a[text()='Gmail']")).click();
+        getButton().click();
     }
 
 }
